478caf5ce7ee60346a739da0db1891a641f7911b
dgayden-coder <dgayden@dallasisd.org>
2026-06-08 19:31:09 +0000

    Made Guessing Game

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,28 @@
 import streamlit as st
-
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+import random
+st.title("🎈 Can You Guess IT?")
+st.subheader(
+    "Let's see if you can guess the correct option"
 )
+
+if 'secret' not in st.session_state:
+    st.session_state.secret = random.randint(1,50)
+
+
+guess = st.number_input('input a number between 1-50',min_value=1, max_value=50, step=1)
+
+if st.button('Submit Guess'):
+    st.write('You guessed', str(guess))
+    if guess < st.session_state.secret:
+        st.warning('Sorry, too low!')
+    elif guess > st.session_state.secret:
+        st. warning('Sorry, too high!')
+    else:
+        st.success('Great Job! You Rock!')
+
+
+if guess==st.session_state.secret:
+    if st.button ('Play Again'):
+        st.session_state.secret = random.randint (1,50)
+        st.rerun()
+    
